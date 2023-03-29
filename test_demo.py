aa07bbc8436ecb8a70ec88e02c7bb3b89d03605c
Ceyhun Taşcı <ceytas455@gmail.com>
2023-03-29 17:21:40 +0300

    last updated

--- test_demo.py
+++ test_demo.py
@@ -8,13 +8,15 @@ from selenium.webdriver.common.action_chains import ActionChains
 import pytest
 from pathlib import Path
 from datetime import date
+import openpyxl
+from constants import globalConstants
 
 class Test_DemoClass:
     #her testten önce çağrılır
     def setup_method(self):
         self.driver=webdriver.Chrome(ChromeDriverManager().install())
         self.driver.maximize_window
-        self.driver.get("https://www.saucedemo.com/")
+        self.driver.get(globalConstants.URL)
         self.folderPath=str(date.today())
         Path(self.folderPath).mkdir(exist_ok=True)
         #34.03.2023
@@ -33,8 +35,24 @@ class Test_DemoClass:
     #setup -> test_demo2 -> teardown
     def test_demo2(self):
         assert True
+    
+    def getData():
+        #veriyi al
+        excelFile = openpyxl.load_workbook("data/invalid_login.xlsx")
+        selectedSheet = excelFile["Sheet1"]
         
-    @pytest.mark.parametrize("username,password",[("1","1"),("kullaniciadim","sifrem")])
+        totalRows=selectedSheet.max_row
+        data=[]
+        for i in range(2, totalRows+1):
+            username = selectedSheet.cell(i,1).value
+            password = selectedSheet.cell(i,2).value
+            tupleData=(username,password)
+            data.append(tupleData)
+            
+        return data
+        
+        
+    @pytest.mark.parametrize("username,password",getData())
     def test_invalid_login(self,username,password):
         self.waitForElementVisible(By.ID,"user-name")
         usernameInput = self.driver.find_element(By.ID, "user-name")
@@ -46,6 +64,7 @@ class Test_DemoClass:
         loginBtn.click()
         errorMessage = self.driver.find_element(By.XPATH,"//*[@id='login_button_container']/div/form/div[3]/h3")
         self.driver.save_screenshot(f"{self.folderPath}/test-invalid-login-{username}-{password}.png")
+        #magic string
         assert errorMessage.text == "Epic sadface: Username and password do not match any user in this service"
         
     def waitForElementVisible(self,locator,timeout=5):
